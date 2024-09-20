340fdfaa2f904b67073228a6779f11a5f601f203
poooootiful <151514311+poooootiful@users.noreply.github.com>
2024-09-20 15:53:36 +0200

    Update secondpage.dart

--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -23,15 +23,14 @@ class _SecondPageState extends State<SecondPage> {
             child: Column (
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Image.network('https://img.freepik.com/free-vector/ship-with-sails-grunge-poster-design_460848-14474.jpg?ga=GA1.1.1787706498.1726823049&semt=ais_hybrid'),
-                Image.network('https://img.freepik.com/free-vector/skull-pirate-with-cross-bones-vector-illustration_43623-1085.jpg?ga=GA1.1.1787706498.1726823049&semt=ais_hybrid'),
-                Image.network('https://img.freepik.com/free-vector/skull-pirates-captain-vector-illustration_43623-1287.jpg?ga=GA1.1.1787706498.1726823049&semt=ais_hybrid'),
-                Image.network('https://img.freepik.com/free-vector/monochrome-vintage-skull_225004-1671.jpg?ga=GA1.1.1787706498.1726823049&semt=ais_hybrid'),
-                Image.network('https://img.freepik.com/free-vector/monochrome-pirate-skull_225004-109.jpg?ga=GA1.1.1787706498.1726823049&semt=ais_hybrid'),
-                Image.network('https://img.freepik.com/free-vector/skull-with-pipe_225004-1744.jpg?ga=GA1.1.1787706498.1726823049&semt=ais_hybrid'),
-                Image.network('https://img.freepik.com/free-vector/vintage-monochrome-skull-pirate-hat_225004-2011.jpg?ga=GA1.1.1787706498.1726823049&semt=ais_hybrid'),
-                Image.network('https://img.freepik.com/free-vector/vintage-skull-pirate-hat_225004-103.jpg?ga=GA1.1.1787706498.1726823049&semt=ais_hybrid'),
-                Image.network('https://img.freepik.com/free-vector/skull-smoke-cloud_225004-1812.jpg?ga=GA1.1.1787706498.1726823049&semt=ais_hybrid')
+                Image.network('https://images.freeimages.com/image/previews/d56/pirate-crab-cartoon-png-5692811.png'),
+                Image.network('https://images.freeimages.com/image/previews/a61/croco-pirate-character-png-5692813.png?fmt=webp&w=500'),
+                Image.network('https://images.freeimages.com/image/previews/e4f/pirate-sloth-cartoon-png-5692812.png?fmt=webp&w=500'),
+                Image.network('https://images.freeimages.com/image/previews/8f0/spider-pirate-cartoon-character-5692820.png?fmt=webp&w=500'),
+                Image.network('https://images.freeimages.com/image/previews/6f3/pirate-octo-cartoon-png-5692815.png?fmt=webp&w=500'),
+                Image.network('https://images.freeimages.com/image/previews/54c/pirate-elephant-cartoon-png-5692810.png?fmt=webp&w=500'),
+                Image.network('https://images.freeimages.com/image/previews/6cb/pirate-mouse-cartoon-png-5692817.png?fmt=webp&w=500'),
+                Image.network('https://images.freeimages.com/image/previews/1eb/fox-pirate-character-png-art-5692818.png?fmt=webp&w=500')
               ],
             )
           ),
